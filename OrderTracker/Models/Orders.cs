b172b25efe93f78bbd9fcae5219ca8268c8b1209
ambi hidalgo <ambio.pk@gmail.com>
2023-02-25 13:50:16 -0800

    Add Order Constructor to be tested

--- OrderTracker/Models/Orders.cs
+++ OrderTracker/Models/Orders.cs
@@ -1,1 +1,21 @@
 using System.Collections.Generic;
+
+namespace Tracker.Models
+{
+  public class Orders
+  {
+    public string Product {get; set;}
+    public string Quantity {get; set;}
+    public string Date {get; set;}
+    public int ID {get;}
+    private int _rollingCounter = new int();
+
+    public Orders (string product, string quantity, string date){
+      Product = product;
+      Quantity = quantity;
+      Date = date;
+      _rollingCounter++;
+      ID = _rollingCounter;
+    }
+  }
+}
