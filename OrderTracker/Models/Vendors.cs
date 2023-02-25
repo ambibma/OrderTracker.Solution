1cee87c30def60cfb61e65422cab373e3eeb63a0
ambi hidalgo <ambio.pk@gmail.com>
2023-02-25 13:49:22 -0800

    Add Vendors Constructor To Be Tested

--- OrderTracker/Models/Vendors.cs
+++ OrderTracker/Models/Vendors.cs
@@ -2,12 +2,25 @@ using System.Collections.Generic;
 
 namespace Tracker.Models
 {
-  public class Vendors
+  public class Vendor
   {
     public string Name {get; set;}
     public string Description {get; set;}
-    public Order Information {get; set;}
-    
+    public string Cost {get; set;}
+    public int ID {get;}
+    private static int _rollingCounter = new Int();
 
+    public Orders Information {get; set;}
+    private static List<Vendor> _instances = new List<Vendor>{};
+  
+    public Vendor (string name, string description, string cost, Orders inputOrder){
+      Name = name;
+      Description = description;
+      Cost = cost;
+      _rollingCounter++;
+      ID = _rollingCounter;
+      Information = inputOrder;
+      _instances.Add(this);
+    }
   }
 }
