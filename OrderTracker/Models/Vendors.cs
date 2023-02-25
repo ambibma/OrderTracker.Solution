d999a1f04c33fd5ea81d82556959f2fd4ab7df2d
ambi hidalgo <ambio.pk@gmail.com>
2023-02-25 00:46:00 -0800

    WIP Add Vendor constructor

---
+++ OrderTracker/Models/Vendors.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Tracker.Models
+{
+  public class Vendors
+  {
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public Order Information {get; set;}
+    
+
+  }
+}
