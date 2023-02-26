59210b8caf070153b048c34a8ae5ff84a7ec96ac
ambi hidalgo <ambio.pk@gmail.com>
2023-02-26 02:56:27 -0800

    Remove empty space

--- OrderTracker/Controllers/Orders.cs
+++ OrderTracker/Controllers/Orders.cs
@@ -13,18 +13,7 @@ namespace Tracker.Controllers
        Vendor vendor = Vendor.Find(vendorId);
        return View(vendor);
     }
-    // [HttpPost("/vendors/{vendorId}/orders")]
-    // public ActionResult Create(int vendorId, string orderProduct, string orderQuantity, string orderDate)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Vendor foundVendor = Vendor.Find(vendorId);
-    //   Order newOrder = new Order(orderProduct, orderQuantity, orderDate);
-    //   foundVendor.AddOrder(newOrder);
-    //   List<Order> vendorOrders = foundVendor.Orders;
-    //   model.Add("orders", vendorOrders);
-    //   model.Add("vendor", foundVendor);
-    //   return View("Show", model);
-    // }
+    
     [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
     public ActionResult Show(int vendorId, int orderId)
     {
